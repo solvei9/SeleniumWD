60fa9042ad199714e5da32e4fd29493f8e20ac4d
Makarenko Ekaterina <e.makarenko@cft.ru>
2018-04-16 13:16:56 +0700

    Lesson1: update simple test.

--- java-example/src/test/java/ru/stqa/training/selenium/lesson1/SimpleTest.java
+++ java-example/src/test/java/ru/stqa/training/selenium/lesson1/SimpleTest.java
@@ -5,6 +5,14 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.*;
+
+import java.io.File;
 
 public class SimpleTest {
     private WebDriver driver;
@@ -12,8 +20,13 @@ public class SimpleTest {
     @Before
     public void start() {
         //System.setProperty("webdriver.chrome.driver", "/home/serene/Downloads/chromedriver"); // Ubuntu
-        System.setProperty("webdriver.chrome.driver", "C:/Tools/chromedriver.exe"); // Windows
-        driver = new ChromeDriver();
+        //System.setProperty("webdriver.chrome.driver", "C:/Tools/chromedriver.exe"); // Windows
+        //driver = new ChromeDriver();
+        //driver = new InternetExplorerDriver();
+        //DesiredCapabilities caps = new DesiredCapabilities();
+        //caps.setCapability(FirefoxDriver.MARIONETTE, true);
+        //driver = new FirefoxDriver(new FirefoxBinary(new File("C:\\Program Files\\Firefox Nightly\\firefox.exe")), new FirefoxProfile(), caps);
+        driver = new FirefoxDriver();
     }
 
     @Test
