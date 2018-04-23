c2326fc385c6fb0bbaed3cd45091f6a06890791d
Makarenko Ekaterina <e.makarenko@cft.ru>
2018-04-23 17:05:44 +0700

    Task10: add a draft.

---
+++ java-example/src/test/java/ru/stqa/training/selenium/lesson5/Task10.java
@@ -0,0 +1,41 @@
+package ru.stqa.training.selenium.lesson5;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class Task10 {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        // System.setProperty("webdriver.chrome.driver", "/home/serene/Downloads/chromedriver"); // Ubuntu
+        System.setProperty("webdriver.chrome.driver", "C:/Tools/chromedriver.exe"); // Windows
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver, 2);
+        // Открытие страницы
+        driver.get("http://localhost/litecart");
+    }
+
+    @Test
+    public void productTest() {
+        WebElement product = driver.findElement(By.cssSelector("dv#box-campaigns "));
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
