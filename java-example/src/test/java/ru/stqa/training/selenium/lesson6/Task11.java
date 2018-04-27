8121c225e9e937150185857d8f10c272b62aa083
Makarenko Ekaterina <e.makarenko@cft.ru>
2018-04-27 11:42:42 +0700

    Task11: delete unused import.

--- java-example/src/test/java/ru/stqa/training/selenium/lesson6/Task11.java
+++ java-example/src/test/java/ru/stqa/training/selenium/lesson6/Task11.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
