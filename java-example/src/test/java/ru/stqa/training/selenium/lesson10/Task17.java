118949dd06d75ae36c67d64f5bf805d26dbd65e4
Makarenko Ekaterina <e.makarenko@cft.ru>
2018-05-08 15:51:55 +0700

    Task17: add draft.

---
+++ java-example/src/test/java/ru/stqa/training/selenium/lesson10/Task17.java
@@ -0,0 +1,46 @@
+package ru.stqa.training.selenium.lesson10;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class Task17 {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        // System.setProperty("webdriver.chrome.driver", "/home/serene/Downloads/chromedriver"); // Ubuntu
+        // System.setProperty("webdriver.chrome.driver", "C:/Tools/chromedriver.exe"); // Windows
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 5);
+        // Открытие страницы
+        driver.get("http://localhost/litecart/admin");
+        // Вход под пользователем admin
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+        wait.until(titleIs("My Store"));
+        // Переход к странам
+        driver.get("http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=1");
+        wait.until(titleIs("Catalog | My Store"));
+    }
+
+    @Test
+    public void logTest() {
+        Assert.assertEquals(driver.manage().logs().get("browser").getAll().size(), 0);
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
